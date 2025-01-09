128c6be7f43ebbc4dedaf275219d08be4e26c907
Jonathan <coxjonathan2016@gmail.com>
2025-01-08 23:08:08 -0700

    Updated get_weather_data and store_weather_data

--- python/get_weather_data.py
+++ python/get_weather_data.py
@@ -1,7 +1,7 @@
 import requests
 import json
 
-def get_temperature(location_name, latitude: float, longitude:float):
+def get_temperature_data(location_name, latitude: float, longitude:float):
     # API call
     url = f"https://api.open-meteo.com/v1/forecast?latitude={latitude}&longitude={longitude}&current=temperature_2m&temperature_unit=fahrenheit"
 
@@ -9,16 +9,15 @@ def get_temperature(location_name, latitude: float, longitude:float):
     try:
         response = requests.get(url)
         if response.status_code == 200:
-            temperature = response.json()
-            return temperature
+            temperature = response.json()['current']['temperature_2m']
         else:
             print("Error: ", response.status_code)
             return None
     except Exception as e:
         print(f"Unexpected error: {e}")
         return None
+    return [location_name, latitude, longitude, temperature]
 
-response = get_temperature(40.7188,-111.8505)
-print(response)
-temperature = response['current']['temperature_2m']
-print(temperature)
+response = get_temperature_data("Salt Lake City",40.4000,-111.8505)
+#temperature = response['current']['temperature_2m']
+#print(temperature)
--- python/store_weather_data.py
+++ python/store_weather_data.py
@@ -1,5 +1,5 @@
 import mysql.connector
-from get_weather_data import get_temperature
+from get_weather_data import get_temperature_data
 
 connection = mysql.connector.connect(
     host='localhost',
@@ -9,3 +9,18 @@ connection = mysql.connector.connect(
 )
 
 cursor = connection.cursor()
+
+data = get_temperature_data("Salt Lake City",40.4000,-111.8505)
+
+insert_query = """
+    INSERT INTO temperature(city_name, latitude, longitude, curr_temperature)
+    VALUES (%s, %s, %s, %s)
+"""
+
+cursor.execute(insert_query, data)
+connection.commit()
+
+print(f"{cursor.rowcount} rows inserted.")
+
+cursor.close()
+connection.close()
